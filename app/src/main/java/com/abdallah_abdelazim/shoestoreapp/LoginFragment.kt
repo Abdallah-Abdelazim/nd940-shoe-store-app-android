98163dc89f6bf2775c4166c46bff6c66de40c11e
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2022-12-23 15:44:54 +0200

    Finish `LoginFragment` UI

--- app/src/main/java/com/abdallah_abdelazim/shoestoreapp/LoginFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/shoestoreapp/LoginFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.abdallah_abdelazim.shoestoreapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding
+        get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
     }
 
     companion object {
