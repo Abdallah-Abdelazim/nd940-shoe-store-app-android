da6608dfb8568a5c6c2d71f3acad4813af5329bd
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2022-12-23 22:54:29 +0200

    Finish `WelcomeFragment`

--- app/src/main/java/com/abdallah_abdelazim/shoestoreapp/onboarding/WelcomeFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/shoestoreapp/onboarding/WelcomeFragment.kt
@@ -4,17 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.abdallah_abdelazim.shoestoreapp.R
+import com.abdallah_abdelazim.shoestoreapp.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_welcome, container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        attachUiListeners()
+    }
+
+    private fun attachUiListeners() {
+        binding.btnContinue.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
+        }
     }
 
     companion object {
